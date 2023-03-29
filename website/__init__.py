3c433612f5219ae4d07541a8dc09c93302dcbb04
umeed-salman <89387431+umeed-salman@users.noreply.github.com>
2023-03-29 19:50:43 +0500

    Add files via upload

---
+++ website/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'd54615cba0239e9cbc33262c'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+from website import routes
